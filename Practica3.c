0a1cf82e5fc673e08d3f01744c332de28b1ebb98
Luis Ángel <101238265+ThirtforBep@users.noreply.github.com>
2022-12-12 22:28:55 -0600

    Update Practica3.c

--- Practica3.c
+++ Practica3.c
@@ -1,348 +1,229 @@
-#include<stdlib.h>
-#include<stdio.h>
-#include<unistd.h>
-#include<time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-#define TAM16 16
-#define TAM8 8
-#define TAM7 7
+//BITS NECESARIOS PARA CONVERTIR DECIMAL A BINARIO; TAMAÑO DE LASUNIDADES DE MEMORIA -------
 
-//Estructura memoria Fisica
-typedef struct MemoriaFisica MarcoPaginas;
-struct MemoriaFisica{
+//BITS
+#define DECIMAL 4096
+#define BITS_DECIMAL 11
+#define BITS_PAGINAS_MV 3
+#define BITS_MARCOS_MF 2
 
-    int NumMa;
-    int Direc;
-    int Age;
-    MarcoPaginas *sig;
-};
+//TAMAÑOS DE LA MEMORIA
+#define MV_SIZE 16
+#define MF_SIZE 8
+#define UNIDADES_MEMORIA 4096
 
-//Estructura Memory
-typedef struct Memory Pages;
-struct Memory{
-    int Direc;
-    int NumPa;
-    Pages *sig;
-};
+//ESTRUCTURAS DE LAS LISTAS_GENERALES -------------------------------------------------------
+typedef struct lista_general{
+//INDICE DE PROPIO E INDICE AJENO
+int my_indice;
+int its_indice;
+int estado;
+//PUNTERO A LISTA ENLAZADA
+struct lista_general *sig_list;
+}lista;
+typedef lista *punterolista;
 
-void *PaginaNueva();
-void *MarcoNuevo();
-void MarcoPagIns(MarcoPaginas **Puntero, int numMarco, int numDirec, int edad);
-void AgregarPag(Pages **Puntero, int numPag, int numDirec);
-void ImpMV(Pages *Puntero);
-void ImpMF(MarcoPaginas *Puntero);
-int Num(int posicion, int Vector[]);
-void Mal(int numPag, Pages **Puntero1, MarcoPaginas **Puntero2);
-void ImBin(int num);
-void MDirec(int selecciono, MarcoPaginas **Puntero2);
+//PROTOTIPOS DE LAS FUNCIONES ----------------------------------------------------------------
+void insertar (punterolista *cabeza, int e);
+void imprimir_mv (punterolista cabeza);
+void imprimir_mf (punterolista cabeza);
+//void borrarLista (tpuntero *cabeza);
+void binario4096(int i);
+void binarioPagina(int i);
+void binarioMarcos(int i);
+void ocupacion(int mv, int mf, punterolista aux_mv_cabeza, punterolista aux_mf_cabeza);
+//FUNCIONES ESTETICAS
+void espera(int i);
 
+//FUNCION MAIN ------------------------------------------------------------------------------
 int main(){
-    Pages *Memoria64k = NULL;
-    MarcoPaginas *Memoria32k = NULL;
-    int posicion, Vector[TAM16], VectorEdad[TAM16], NumePag, Opcion, Selecciono, edad;
-    system("clear");
-    srand(time(NULL));
-    for(int i = 0; i < TAM16; i++){
-        do{
-            posicion = 1+rand()%TAM16;
-        }while(Num(posicion, Vector));{
-            Vector[i] = posicion;
-            if(posicion >= 9){
-            AgregarPag(&Memoria64k, i, -1);
-            }else{
-                do{
-                edad = 1+rand()%50;
-                }while(Num(edad, VectorEdad));{
-                    VectorEdad[i] = edad;
-                    AgregarPag(&Memoria64k, i, posicion -1);
-                    MarcoPagIns(&Memoria32k, i, posicion - 1 , edad);
-                }
-            }
-        }
-    }
+    int i;
+    int seleccion;
+    int mv, mf;
+    punterolista mv_cabeza; //Indica la cabeza de la lista enlazada, si la perdemos no
+    podremos acceder a la lista
+    punterolista mf_cabeza;
+    mv_cabeza = NULL; //Se inicializa la cabeza como NULL ya que no hay ningun nodo
+    cargado en la lista
+    mf_cabeza = NULL;
+    for(i=0;i<MV_SIZE;i++)
+    insertar(&mv_cabeza, i);
+    for(i=0;i<MF_SIZE;i++)
+    insertar(&mf_cabeza, i);
+    printf("\tESTO ES UN SIMULACRO!!!!!!!! ESTO ES UN SIMULACRO!!!!!!!!\n");
+    printf("\tNINGUNA PARTE DE LA MEMORIA SERA DAÑADA DURANTE EL PROCESO DE ESTE SIMULACRO\n");
+    //espera(3);
+    printf("Bueno la verdad dañe poquito mi disco al principio xc\n");
+    //espera(3);
+    printf("Tamos haciendo las paginas espere :3\n");
+    //espera(3);
+    printf("\nCON USTEDES!!!! LAS PAGINAS!!!!! \n");
+    //espera(1);
+    imprimir_mv(mv_cabeza);
+    printf("\n\t Y AHORA... LOS MARCOS PARA NUESTRAS PAGINAS\n");
+    //espera(3);
+    imprimir_mf(mf_cabeza);
+    printf("\n\t AHORA LLEGO EL MOMENTO DE LA VERDAD USTED VA A DECIDIR QUE HACER CON El DESTINO DE LAS PAGINAS Y EN QUE MARCO SE QUEDARA CADA PAGINA\n ");
+    printf("\t (que emocion, que nervios)");
+    //espera(3);
     do{
+        printf("\n\t DENTRO DE LA CATAFIXIA TENEMOS 3 OPCIONES, OKEY NO SOLO SON 2 NO HAY PRESUPUESTO \"F\"\n\n PERO DIGAME SEÑOR AGUILERA QUE TENEMOS!!!!!!! ");
+        printf("\n[1] TENEMOS UN MARCO PARA UNA PAGINA!!");
+        printf("\n[2] Con esta opcion puedes ver las paginas y los marcos :3");
+        printf("\n[3] Salir :)\n");
+        scanf("%d", &seleccion);
+        if(seleccion==1){
+            printf("\n-------------------------------------*MODO SERIO-------------------------------------");
+            printf("\n\nMuy bien ahora necesito que me diga el indice de memoria VIRTUAL (pagina) quiere mover: ");
+            scanf("%d",&mv);
+            printf("\n\nMuy bien ahora necesito que me diga el indice de memoria FISICA (marco) que quiere ocupar: ");
+            scanf("%d",&mf);
+            printf("\nVamos a trabajar en esto");
+            ocupacion(mv,mf,mv_cabeza,mf_cabeza);
+        }
+        if(seleccion == 2){
+            imprimir_mv(mv_cabeza);
+            espera(1);
+            imprimir_mf(mf_cabeza);
+        }
+        else
+        printf("\nHAIGA LAS COSAS VIEN NO CEA MENZO");
+    }while(seleccion!=3);
+    //printf ("\nSe borra la lista cargada\n");
+    //borrarLista (&cabeza);
+    return 0;
+}
+
+//FUNCIONES CREADAS ------------------------------------------------------------------------
+
+//FUNCIONES PARA LISTAS
+void insertar (punterolista *cabeza, int e){
+    punterolista nuevo; //Creamos un nuevo nodo
+    nuevo = malloc(sizeof(lista)); //Utilizamos malloc para reservar memoria para ese nodo
+    nuevo->my_indice = e; //Le asignamos el valor ingresado por pantalla a ese nodo
+    nuevo->estado = 1;
+    nuevo->sig_list = *cabeza; //Le asignamos al siguiente el valor de cabeza
+    *cabeza = nuevo; //Cabeza pasa a ser el ultimo nodo agregado
+}
+
+//IMPRIMIR EN PANTALLA
+void imprimir_mv(punterolista cabeza){
+    printf("Indice\tMV\t\tMF\t\tESTADO\n");
+    while(cabeza != NULL){ //Mientras cabeza no sea NULL
+        printf("%d\t",cabeza->my_indice);
+        binarioPagina(cabeza->my_indice);
+        printf(" / %d", cabeza->my_indice);
+        printf("\t");
+        binarioMarcos(cabeza->its_indice);
+        printf(" / %d",cabeza->its_indice);
+        printf("\t\t %d",cabeza->estado);
         printf("\n");
+        cabeza = cabeza->sig_list; //Pasamos al siguiente nodo
+    }
+}
+void imprimir_mf(punterolista cabeza){
+    printf("Indice\tMV\t\tMF\t\tESTADO\n");
+    while(cabeza != NULL){ //Mientras cabeza no sea NULL
+        printf("%d\t",cabeza->my_indice);
+        binarioMarcos(cabeza->my_indice);
+        printf(" / %d", cabeza->my_indice);
+        printf("\t\t");
+        binarioPagina(cabeza->its_indice);
+        printf(" / %d",cabeza->its_indice);
+        printf("\t\t %d",cabeza->estado);
+        printf("\n");
+        cabeza = cabeza->sig_list; //Pasamos al siguiente nodo
+    }
+}
+/*void borrarLista(tpuntero *cabeza){
+tpuntero actual; //Puntero auxiliar para eliminar correctamente la lista
+while(*cabeza != NULL){ //Mientras cabeza no sea NULL
+actual = *cabeza; //Actual toma el valor de cabeza
+*cabeza = (*cabeza)->sig; //Cabeza avanza 1 posicion en la lista
+free(actual); //Se libera la memoria de la posicion de Actual (el primer nodo), y cabeza
+queda apuntando al que ahora es el primero
+}
+}
+*/
 
-        ImpMV(Memoria64k);
-        ImpMF(Memoria32k);
-
-        printf( "\n\t\t\t\t\t\t\tMP/Marco de pagina\n\t\t\t\t\t\t\tPA -> Pagina\n\n");
-
-        printf("\t\t\t\t\t\t\t1) Agregar una pagina\n");
-        printf("\t\t\t\t\t\t\t2) Visualizar direcciones de memoria\n");
-        printf("\t\t\t\t\t\t\t3) Exit\n");
-        printf("\t\t\t\t\t\t\tOpcion: ");
-        scanf("%d", &Opcion);
-        switch(Opcion){
-        case 1:
-            printf("\t\t\t\t\t\t\tPagina que desea pasar a la pagina ");
-            scanf("%d", &Selecciono);
-            Mal(Selecciono, &Memoria64k,
-            &Memoria32k);
-            break;
-        case 2:
-            printf("\t\t\t\t\t\t\tD que marco de pagina dessea visualizar sus direcciones ");
-            scanf("%d", &Selecciono);
-            MDirec(Selecciono, &Memoria32k);
-            break;
-        default:
-            printf("\t\t\t\t\t\t Ingresa otra opcion\n");
-            sleep(2);
-            break;
-        }
-    }while(Opcion != 3);{
-        printf("Fin\n");
+//FUNCION DE CONVERSION DE DECIAMAL A BINARIO CREDITOS: EJEMPLO1
+void binario4096(int i){
+    int bits = BITS_DECIMAL;
+    while(bits >= 0){
+        if(i & (((long int) 1) << bits))
+        printf("1");
+        else
+        printf("0");
+        bits--;
     }
 }
 
-void *PaginaNueva(){
-    Pages Nueva = (Pages)malloc(sizeof(Pages));
-    if(Nueva == NULL){
-        perror("No se puede crear\n");
-    }else{
-        return Nueva;
+void binarioPagina(int i){
+    int bits = BITS_PAGINAS_MV;
+    while(bits >= 0){
+    if(i & (((long int) 1) << bits))
+        printf("1");
+    else
+        printf("0");
+    bits--;
     }
 }
 
-void *MarcoNuevo(){
-    MarcoPaginas *Nueva = (MarcoPaginas*)malloc(sizeof(MarcoPaginas));
-    if(Nueva == NULL){
-        perror("No se puede crear pagina\n");
-    }else{
-        return Nueva;
+void binarioMarcos(int i){
+    int bits = BITS_MARCOS_MF;
+    while(bits >= 0){
+        if(i & (((long int) 1) << bits))
+        printf("1");
+        else
+        printf("0");
+        bits--;
     }
 }
 
-
-void MarcoPagIns(MarcoPaginas **Puntero, int numMarco, int numDirec, int edad){
-
-    MarcoPaginas *Actual, *Anterior, *pNuevo, *pPrimeraMarco;
-    pPrimeraMarco = *Puntero;
-    Actual = pPrimeraMarco;
-    Anterior = pPrimeraMarco;
-    if(pPrimeraMarco == NULL){
-        pPrimeraMarco = MarcoNuevo();
-        pPrimeraMarco->NumMa = numMarco;
-        pPrimeraMarco->Direc = numDirec;
-        pPrimeraMarco->Age = edad;
-        pPrimeraMarco->sig = NULL;
-        *Puntero = pPrimeraMarco;
-    }else{
-        while(Actual != NULL && numDirec < Actual->Direc){
-            Anterior = Actual;
-            Actual = Actual->sig;
-        }
-        pNuevo = MarcoNuevo();
-        pNuevo->NumMa = numMarco;
-        pNuevo->Direc = numDirec;
-        pNuevo->Age = edad;
-        if(Anterior == Actual){
-            pNuevo->sig = pPrimeraMarco;
-            pPrimeraMarco = pNuevo;
-        }else{
-            pNuevo->sig = Actual;
-            Anterior->sig = pNuevo;
-        }
-        *Puntero = pPrimeraMarco;
+//FUNCIONES ESTETICAS
+void espera(int i){
+    while(i){
+        printf(".");
+        sleep(i);
+        i--;
     }
+    printf("\n");
 }
 
-
-void AgregarPag(Pages **Puntero, int numPag, int numDirec){
-
-    Pages *Actual, *Anterior, *pNuevo, *pPrimeraPag;
-    pPrimeraPag = *Puntero;
-    Actual = pPrimeraPag;
-    Anterior = pPrimeraPag;
-    if(pPrimeraPag == NULL){
-        pPrimeraPag = PaginaNueva();
-        pPrimeraPag->NumPa = numPag;
-        pPrimeraPag->Direc = numDirec;
-        pPrimeraPag->sig = NULL;
-        *Puntero = pPrimeraPag;
-    }else{
-        while(Actual != NULL && numPag < Actual->NumPa){
-            Anterior = Actual;
-            Actual = Actual->sig;
-        }
-        pNuevo = PaginaNueva();
-        pNuevo->NumPa = numPag;
-        pNuevo->Direc = numDirec;
-        if(Anterior == Actual){
-        pNuevo->sig = pPrimeraPag;
-        pPrimeraPag = pNuevo;
-        }else{
-            pNuevo->sig = Actual;
-            Anterior->sig = pNuevo;
-        }
-        *Puntero = pPrimeraPag;
-    }
-}
-
-
-void ImpMV(Pages *Puntero){
-
-    Pages *Auxiliar;
-    int Num;
-    Auxiliar = Puntero;
-    if(Auxiliar == NULL){
-        perror("Sin memoria\n");
-    }else{
-        printf("\t\t\t\t\t\t\tMemory Virtual \n");
-        printf( "\t\t\t\t\t\t\t\t|PA|MP|\n");
-        while(Auxiliar != NULL){
-            if(Auxiliar->Direc != -1){
-                printf( "\t\t\t\t\t\t\t\t %.2d %.2d",
-                Auxiliar->NumPa, Auxiliar->Direc);
-            }else{
-                printf("\t\t\t\t\t\t\t\t %.2d %s",
-                Auxiliar->NumPa, "xx");
-            }
-            printf("\n");
-            Auxiliar = Auxiliar->sig;
-        }
-    }
-}
-
-
-void ImpMF(MarcoPaginas *Puntero){
-    MarcoPaginas *Auxiliar;
-    int Num;
-    Auxiliar = Puntero;
-    if(Auxiliar == NULL){
-        perror("Sin memoria\n");
-    }else{
-        printf( "\t\t\t\t\t\t\tMemory Fisica\n");
-        printf( "\t\t\t\t\t\t\t\t|MP|PA|\n");
-        while(Auxiliar != NULL){
-            printf( "\t\t\t\t\t\t\t\t %.2d %.2d",
-            Auxiliar->Direc, Auxiliar->NumMa);
-            printf("\n");
-            Auxiliar = Auxiliar->sig;
-        }
-    }
-}
-
-int Num(int posicion, int Vector[]){
-
-    int bandera = 0;
-    for(int i = 0; i < TAM16; i++){
-        if(posicion == Vector[i]){
-            bandera = 1;
-        }
-    }
-    return bandera;
-}
-
-int Bin(int Num){
-
-    if(Num > 1){
-        Bin(Num/2);
-    }
-    printf("%d", Num%2);
-}
-
-void Mal(int numPag, Pages **Puntero1, MarcoPaginas **Puntero2){
-
-    Pages *MemoriaVirtual, *Anterior, *Actual;
-    MarcoPaginas *MemoriaFisica, *AnteriorFisca, *ActualFisica, *Auxiliar;
-    int NumPag, NumDirec, NumMarco, edadMenor = 0, edad;
-    MemoriaVirtual = *Puntero1;
-    Anterior = MemoriaVirtual;
-    Actual = MemoriaVirtual;
-
-    if(MemoriaVirtual == NULL){
-        perror("no se selecciono la memoria virtual\n");
-    }else{
-        while(Actual != NULL && numPag != Actual->NumPa){
-        Anterior = Actual;
-        Actual = Actual->sig;
-        }
-        if(Actual == NULL){
-        perror("Dato no encontrado\n");
-        }else{
-            if(Actual->Direc != -1){
-            printf("Pagina en memoria\n");
-            sleep(2);
-            }else{
-                NumPag = Actual->NumPa;
-                MemoriaFisica = *Puntero2;
-                AnteriorFisca = MemoriaFisica;
-                ActualFisica = MemoriaFisica;
-                Auxiliar = MemoriaFisica;
-
-                if(MemoriaFisica == NULL){
-                    perror("Sin memoria fisica\n");
-                }else{
-                    while(Auxiliar != NULL){
-                        if(Auxiliar->Age > edadMenor){
-                        edadMenor = Auxiliar->Age;
-                        }
-                        Auxiliar = Auxiliar->sig;
-                    }
-                    while(ActualFisica != NULL && edadMenor != ActualFisica->Age){
-                        AnteriorFisca = ActualFisica;
-                        ActualFisica =
-                        ActualFisica->sig;
-                    }
-                    if(ActualFisica == NULL){
-                        perror("Dato no encontrado\n");
-                    }else{
-                        edad = 1+rand()%50;
-                        ActualFisica->NumMa = NumPag;
-                        ActualFisica->Age = edad;
-                        NumMarco = ActualFisica->Direc;
+void ocupacion(int mv, int mf, punterolista aux_mv_cabeza, punterolista aux_mf_cabeza){
+    //printf("\nSi entro a ocuapcion");
+    int i;
+    //punterolista *aux_mv_cabeza = m:v_cabeza;
+    //punterolista *aux_mf_cabeza = mf_cabeza;
+    while(aux_mv_cabeza!= NULL){
+        if(aux_mv_cabeza->my_indice == mv){
+            while(aux_mf_cabeza != NULL){
+                if(aux_mf_cabeza->my_indice == mf){
+                    aux_mv_cabeza->its_indice = aux_mf_cabeza->my_indice;
+                    aux_mf_cabeza->its_indice = aux_mv_cabeza->my_indice;
+                    aux_mv_cabeza->estado = 0;
+                    aux_mf_cabeza->estado = 0;
+                    //printf("\n%d %d %d",aux_mv_cabeza->my_indice, aux_mv_cabeza->its_indice,
+                    aux_mv_cabeza->estado);
+                    //printf("\n%d %d %d",aux_mf_cabeza->my_indice, aux_mf_cabeza->its_indice,
+                    aux_mf_cabeza->estado);
+                    for(i=aux_mv_cabeza->my_indice * UNIDADES_MEMORIA;i < aux_mv_cabeza->my_indice * UNIDADES_MEMORIA + UNIDADES_MEMORIA;i++){
+                        binarioPagina(aux_mv_cabeza->my_indice);
+                        printf(" ");
+                        binario4096(i);
+                        printf("\t");
+                        binarioMarcos(aux_mf_cabeza->my_indice);
+                        printf(" ");
+                        binario4096(i);
+                        printf("\n");
                     }
                 }
-                *Puntero2 = MemoriaFisica;
+                aux_mf_cabeza = aux_mf_cabeza->sig_list;
             }
-            *Puntero1 = MemoriaVirtual;
         }
-    }
-}
-
-void ImBin(int num){
-    int cont1 = 1, cont2 = 0;
-    while(cont1 != 4097){
-        if(cont1 % 8 == 0){
-            Bin(num);
-            printf("-");
-            Bin(cont2);
-            printf("\n");
-        }else{
-            Bin(num);
-            printf("-");
-            Bin(cont2);
-            printf(" ");
-        }
-        cont2++;
-        cont1++;
-    }
-}
-
-void MDirec(int selecciono, MarcoPaginas **Puntero2){
-    MarcoPaginas *MemoriaFisica, *Anterior, *Actual;
-    int numPag, numMar;
-    MemoriaFisica = *Puntero2;
-    Anterior = MemoriaFisica;
-    Actual = MemoriaFisica;
-    if(MemoriaFisica == NULL){
-        perror("Sin memoria fisica\n");
-    }else{
-        while(Actual != NULL && selecciono != Actual->Direc){
-            Anterior = Actual;
-            Actual = Actual->sig;
-        }
-        if(Actual == NULL){
-            perror("Sin dato\n");
-        }else{
-            numPag = Actual->Direc;
-            numMar = Actual->NumMa;
-            system("clear");
-            printf("\n\n\t\t\t\t\t\tMemory Virtual\n");
-            ImBin(numPag);
-            printf("\n\n\t\t\t\t\t\t\tMemory Fisica\n");
-            ImBin(numMar);
-        }
+        aux_mv_cabeza = aux_mv_cabeza->sig_list;
     }
 }
